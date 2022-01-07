bae24842437e7ef5f8bff6c5d6e12323b3b7f256
Kyle Cho <kylejcho@gmail.com>
2022-01-06 22:13:10 -0500

    Created task class functions

---
+++ src/modules/task.js
@@ -0,0 +1,31 @@
+class Task {
+    constructor(name, notes, dueDate, list) {
+        this.name = name;
+        this.notes = notes;
+        this.dueDate = dueDate;
+        this.list = list;
+    }
+
+    getName(name) {
+        return this.name
+    }
+
+    setName(name) {
+        this.name = name;
+    }
+
+    setNotes(notes) {
+        this.notes = notes;
+    }
+
+    setDueDate(dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    setList(list) {
+        this.list = list;
+    }
+}
+
+
+export default Task;
