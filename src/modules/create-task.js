aad52fa10be89f25f775cb0adbd12f42b1e1a643
Kyle Cho <kylejcho@gmail.com>
2022-01-13 23:56:28 -0500

    Create Task function planning

--- src/modules/create-task.js
+++ src/modules/create-task.js
@@ -1,8 +1,11 @@
 import Task from "./task";
 
 
-const createTask = (task, description, dueDate) => {
-    
+const createTask = (task, description, dueDate, list) => {
+    //add task to general task lists array 
+    //get ui to check date, and list
+    //Determine where to display that task (today, tomorrow, this week, list)
+    //run UI.js function to add task elements to the display.
 }
 
 
