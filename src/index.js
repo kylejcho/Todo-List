f7fb9c18f780782bcd59f3123839f0048bdd5f25
Kyle Cho <kylejcho@gmail.com>
2022-01-17 18:50:12 -0500

    Create task function test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import initialPageLoad from "./modules/ui";
 import Task from "./modules/task";
 import createTask from "./modules/create-task";
-import buttonClicked from "./modules/event-handlers"
+import { formAddButtonClicked } from "./modules/event-handlers";
+import buttonClicked from "./modules/event-handlers";
+
 
 initialPageLoad();
 buttonClicked();
+formAddButtonClicked();
 createTask("Exercise", "Workout out for 45 minutes", "today", '');
 createTask("Read", "Read two chapter", "tomorrow", '');
--- src/modules/event-handlers.js
+++ src/modules/event-handlers.js
@@ -1,6 +1,10 @@
+import createTask from "./create-task";
 
 
 const addButton = document.querySelector('#addButton');
+const formAddButton = document.querySelector('#taskFormAddButton');
+const inputTaskName = document.querySelector('#inputTaskName');
+const inputTaskDescription = document.querySelector('#inputTaskDescription')
 
 const buttonClicked = () => {
     addButton.addEventListener('click', function() {
@@ -9,6 +13,12 @@ const buttonClicked = () => {
     })
 }
 
-
+export const formAddButtonClicked = () => {
+    formAddButton.addEventListener('click', function() {
+        if (inputTaskName.value) {
+            createTask(inputTaskName.value, inputTaskDescription.value)
+        }
+    })
+}
 
 export default buttonClicked;
