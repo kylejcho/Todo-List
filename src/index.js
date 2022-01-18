cd8e52917b71a3c6bf968fe9212c1439caec2b95
Kyle Cho <kylejcho@gmail.com>
2022-01-17 19:27:16 -0500

    Form cancel click

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import initialPageLoad from "./modules/ui";
 import Task from "./modules/task";
 import createTask from "./modules/create-task";
-import { formAddButtonClicked } from "./modules/event-handlers";
+import { formAddButtonClicked, formCancel } from "./modules/event-handlers";
 import buttonClicked from "./modules/event-handlers";
 
 
 initialPageLoad();
 buttonClicked();
 formAddButtonClicked();
+formCancel();
 createTask("Exercise", "Workout out for 45 minutes", "today", '');
 createTask("Read", "Read two chapter", "tomorrow", '');
--- src/modules/event-handlers.js
+++ src/modules/event-handlers.js
@@ -4,19 +4,28 @@ import createTask from "./create-task";
 const addButton = document.querySelector('#addButton');
 const formAddButton = document.querySelector('#taskFormAddButton');
 const inputTaskName = document.querySelector('#inputTaskName');
-const inputTaskDescription = document.querySelector('#inputTaskDescription')
+const inputTaskDescription = document.querySelector('#inputTaskDescription');
+const form = document.querySelector("#taskFormContainer");
 
 const buttonClicked = () => {
     addButton.addEventListener('click', function() {
-        const form = document.querySelector("#taskFormContainer");
-        form.style.visibility = "visible"
+        form.style.visibility = "visible";
     })
 }
 
 export const formAddButtonClicked = () => {
     formAddButton.addEventListener('click', function() {
         if (inputTaskName.value) {
-            createTask(inputTaskName.value, inputTaskDescription.value)
+            createTask(inputTaskName.value, inputTaskDescription.value, "today")
+        }
+    })
+}
+
+export const formCancel = () => {
+    form.addEventListener('click', function(e){
+        console.log(e.target.id)
+        if (e.target.id !== "taskForm" && e.target.id !== "inputTaskName" && e.target.id !== "inputTaskDescription") {
+            form.style.visibility = "hidden";
         }
     })
 }
