cffed120120fa43d8940d3b15cdc5a9429ac014e
Kyle Cho <kylejcho@gmail.com>
2022-01-25 19:32:18 -0500

    SVG path event fix

--- src/modules/event-handlers.js
+++ src/modules/event-handlers.js
@@ -58,20 +58,28 @@ export const selectTask = () => {
 
 export const checkClick = () => {
     document.addEventListener('click', (e)=> {
-        if (e.target.parentNode.className == 'checkContainer') {
+        console.log(e.target)
+        const check = document.querySelector('checkContainer' )
+        if (e.target.parentNode.className == 'checkContainer' ) {
             const checkedTask = e.target.parentNode.parentNode;
-            checkedTask.classList.add('completed');
-            
             const taskName = e.target.parentNode.nextElementSibling;
-            taskName.style.backgroundSize = "100% 100%";
-
-
             const checkContainer = e.target.parentNode;
-            checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>'
-            checkContainer.style.filter = "brightness(0) saturate(100%) invert(74%) sepia(13%) saturate(227%) hue-rotate(177deg) brightness(103%) contrast(97%)";
+            
+            if (!checkedTask.className.includes('completed')) {
+                checkedTask.classList.add('completed');
+                taskName.style.backgroundSize = "100% 100%";
+                checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>'
+                checkContainer.style.filter = "brightness(0) saturate(100%) invert(74%) sepia(13%) saturate(227%) hue-rotate(177deg) brightness(103%) contrast(97%)";
+            } else if (checkedTask.className.includes('completed')){
+                checkedTask.classList.remove('completed');
+                taskName.style.backgroundSize = "0% 100%";
+                checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-q</title><circle cx="256" cy="256" r="192" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>'
+                checkContainer.style.filter = " brightness(0) saturate(100%) invert(9%) sepia(43%) saturate(338%) hue-rotate(177deg) brightness(100%) contrast(96%)";
+            }
         }
     })
 }
+
 
 
 
