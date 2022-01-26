b3917ea55da024ac5ab991cd7bace5144656eca7
Kyle Cho <kylejcho@gmail.com>
2022-01-25 23:07:03 -0500

    Check and delete animations

--- src/index.js
+++ src/index.js
@@ -1,18 +1,13 @@
 import initialPageLoad from "./modules/ui";
 import Task from "./modules/task";
 import { exampleTasks } from "./modules/create-task";
-import { checkClick, formAddButtonClicked, formCancel, selectTask } from "./modules/event-handlers";
-import buttonClicked from "./modules/event-handlers";
+import runEventHandlers from "./modules/event-handlers";
+
 
 
 
 initialPageLoad();
 exampleTasks();
-buttonClicked();
-formAddButtonClicked();
-formCancel();
-selectTask();
-checkClick();
-
+runEventHandlers();
 
 
--- src/modules/event-handlers.js
+++ src/modules/event-handlers.js
@@ -58,32 +58,74 @@ export const selectTask = () => {
 
 export const checkClick = () => {
     document.addEventListener('click', (e)=> {
-        console.log(e.target)
-        const check = document.querySelector('checkContainer' )
         if (e.target.parentNode.className == 'checkContainer' ) {
             const checkedTask = e.target.parentNode.parentNode;
             const taskName = e.target.parentNode.nextElementSibling;
+            const deleteContainer = taskName.nextElementSibling;
             const checkContainer = e.target.parentNode;
             
+            checkContainer.style.animation = "checkClick 0.3s ease-out";
+
             if (!checkedTask.className.includes('completed')) {
                 checkedTask.classList.add('completed');
                 taskName.style.backgroundSize = "100% 100%";
                 checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>'
                 checkContainer.style.filter = "brightness(0) saturate(100%) invert(74%) sepia(13%) saturate(227%) hue-rotate(177deg) brightness(103%) contrast(97%)";
+                deleteContainer.style.opacity = "100";
+                
             } else if (checkedTask.className.includes('completed')){
                 checkedTask.classList.remove('completed');
                 taskName.style.backgroundSize = "0% 100%";
                 checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-q</title><circle cx="256" cy="256" r="192" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>'
                 checkContainer.style.filter = " brightness(0) saturate(100%) invert(9%) sepia(43%) saturate(338%) hue-rotate(177deg) brightness(100%) contrast(96%)";
+                
+
+                setTimeout(()=> {
+                    checkContainer.style.animation = "none";
+                },10)
             }
+            
         }
     })
 }
 
+export const deleteClick = () => {
 
-export const deleteHover = () => {
-    
 }
 
 
-export default buttonClicked;
+
+export const taskContainerHover = () => {
+    document.querySelector('#contentContainer').addEventListener('mouseover', (e)=> {
+        if (e.target.className == 'taskContainer' ) {
+            const taskContainer = e.target;
+            const deleteContainer = taskContainer.children[2];
+            deleteContainer.style.opacity = "100";
+
+            taskContainer.addEventListener('mouseleave', ()=> {
+                if (!taskContainer.className.includes('completed')) {
+                    deleteContainer.style.opacity = '0';
+                }
+                
+            })
+        } 
+        if (e.target.className == 'descriptionContainer' ) {
+            const descriptionContainer = e.target;
+            const deleteContainer = descriptionContainer.previousElementSibling;
+            deleteContainer.style.opacity = "100";
+        } 
+    })
+}
+
+
+const runEventHandlers = () => {
+    checkClick();
+    formAddButtonClicked();
+    formCancel();
+    selectTask();
+    taskContainerHover();
+    deleteClick()
+    buttonClicked()
+}
+
+export default runEventHandlers;
