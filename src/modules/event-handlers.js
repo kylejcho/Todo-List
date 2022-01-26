7e88bc2831a98ad4397b49305a7668a0e36fc5b4
Kyle Cho <kylejcho@gmail.com>
2022-01-25 20:33:13 -0500

    Created delete button for taskcontainer

--- src/modules/event-handlers.js
+++ src/modules/event-handlers.js
@@ -81,8 +81,9 @@ export const checkClick = () => {
 }
 
 
-
-
+export const deleteHover = () => {
+    
+}
 
 
 export default buttonClicked;
--- src/modules/ui.js
+++ src/modules/ui.js
@@ -82,14 +82,20 @@ export const createTaskContainer = (task, description, dueDate) => {
     nameContainer.className ='nameContainer';
     nameContainer.innerText = task;
 
+    const deleteContainer = document.createElement('div');
+    deleteContainer.className = 'deleteContainer';
+    deleteContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-m</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48Zm75.31,260.69a16,16,0,1,1-22.62,22.62L256,278.63l-52.69,52.68a16,16,0,0,1-22.62-22.62L233.37,256l-52.68-52.69a16,16,0,0,1,22.62-22.62L256,233.37l52.69-52.68a16,16,0,0,1,22.62,22.62L278.63,256Z"/></svg>';
+
+
     const descriptionContainer = document.createElement('div');
     descriptionContainer.className = 'descriptionContainer';
     descriptionContainer.innerText = description;
 
     taskContainer.append(checkContainer);
     taskContainer.append(nameContainer);
+    taskContainer.append(deleteContainer);
     taskContainer.append(descriptionContainer);
-
+    
 
     let subGroup;
     if (dueDate == 'today') {
