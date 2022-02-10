166bb25da970b5d7fb69ae236845dd09a7827178
Kyle Cho <kylejcho@gmail.com>
2022-02-10 14:06:49 -0500

    Fixed sidebar overflow

--- src/modules/ui.js
+++ src/modules/ui.js
@@ -64,7 +64,7 @@ export const createTasksContainer = (type) => {
             }
         })
     } else if (type == 'week') {
-        tasksContainerTitle.innerText = "This Week";
+        tasksContainerTitle.innerText = "Next 7 Days";
         createSubGroups("today", tasksContainer, 'title');
         createSubGroups("tomorrow", tasksContainer, 'title');
         allTasks.forEach((task)=> {
@@ -164,7 +164,7 @@ export const createTaskContainer = (task, description, dueDate, status, key, sha
         subGroup = document.querySelector('#upcoming');
     }
 
-    if (status == 'complete') {
+    if (status == 'completed') {
         taskContainer.classList.add('completed');
         taskContainer.children[0].classList.toggle('completed');
         taskContainer.children[0].innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>';
