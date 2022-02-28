59e1157c5781cad6a9f2f3d38a51a509a797c52e
Kyle Cho <kylejcho@gmail.com>
2022-02-28 00:20:10 -0500

    Fixed quickly checking task animation glitch

--- src/modules/animations.js
+++ src/modules/animations.js
@@ -114,40 +114,6 @@ const checkTaskAnimation = (e,a) => {
     const taskViewCheckContainer = document.querySelector('.taskViewCheckContainer');
     const taskViewName = document.querySelector('.taskViewName');
 
-    const taskToggle = (type, state) => {
-        if (type == 'task') {
-            checkedTask.classList.toggle('completed');
-            taskName.classList.toggle('completed');
-            checkContainer.classList.toggle('completed');
-            deleteContainer.classList.toggle('completed');
-            if (state == 'completed') {
-                checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-q</title><circle cx="256" cy="256" r="192" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>'
-            } else {
-                checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>';
-            }
-        } else if (type == 'taskView') {
-            taskViewCheckContainer.classList.toggle('completed');
-            taskViewName.classList.toggle('completed');
-            if (state == 'completed') {
-                taskViewCheckContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 512 512"><title>ionicons-v5-q</title><circle cx="256" cy="256" r="192" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>'
-            } else {
-                taskViewCheckContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>';
-            }
-        }
-    }
-
-    if (!checkedTask.className.includes('completed')) {
-        taskToggle('task');
-        if (taskViewContainer && taskViewContainer.id == 's'+ checkedTask.id) {
-            taskToggle('taskView');
-        }
-    } else if (checkedTask.className.includes('completed')) {
-        taskToggle('task', 'completed');
-        if (taskViewContainer && taskViewContainer.id == 's'+ checkedTask.id) {
-            taskToggle('taskView', 'completed');
-        }
-    }
-
     const taskContainer = taskName.parentNode;
     const subGroup = taskContainer.parentNode;
     const subGroupHeight = subGroup.children.length * 60
@@ -158,9 +124,49 @@ const checkTaskAnimation = (e,a) => {
             startingPoint = i;
         }
     }
+    
+    subGroup.appendChild(taskContainer)
+
+    setTimeout(() => {
+        const taskToggle = (type, state) => {
+            if (type == 'task') {
+                checkedTask.classList.toggle('completed');
+                taskName.classList.toggle('completed');
+                checkContainer.classList.toggle('completed');
+                deleteContainer.classList.toggle('completed');
+                if (state == 'completed') {
+                    checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-q</title><circle cx="256" cy="256" r="192" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>'
+                } else {
+                    checkContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>';
+                }
+            } else if (type == 'taskView') {
+                taskViewCheckContainer.classList.toggle('completed');
+                taskViewName.classList.toggle('completed');
+                if (state == 'completed') {
+                    taskViewCheckContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 512 512"><title>ionicons-v5-q</title><circle cx="256" cy="256" r="192" style="fill:none;stroke:#000;stroke-linecap:round;stroke-linejoin:round;stroke-width:32px"/></svg>'
+                } else {
+                    taskViewCheckContainer.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 512 512"><title>ionicons-v5-e</title><path d="M256,48C141.31,48,48,141.31,48,256s93.31,208,208,208,208-93.31,208-208S370.69,48,256,48ZM364.25,186.29l-134.4,160a16,16,0,0,1-12,5.71h-.27a16,16,0,0,1-11.89-5.3l-57.6-64a16,16,0,1,1,23.78-21.4l45.29,50.32L339.75,165.71a16,16,0,0,1,24.5,20.58Z"/></svg>';
+                }
+            }
+        }
+    
+        if (!checkedTask.className.includes('completed')) {
+            taskToggle('task');
+            if (taskViewContainer && taskViewContainer.id == 's'+ checkedTask.id) {
+                taskToggle('taskView');
+            }
+        } else if (checkedTask.className.includes('completed')) {
+            taskToggle('task', 'completed');
+            if (taskViewContainer && taskViewContainer.id == 's'+ checkedTask.id) {
+                taskToggle('taskView', 'completed');
+            }
+        }
+    }, 10);
+    
 
     taskContainer.style.transition = 'all 0.4s ease-in-out';
-    if (checkedTask.className.includes('completed') && taskContainer.nextElementSibling) {
+    if (!checkedTask.className.includes('completed')) {
+        console.log('hola')
         setTimeout(()=> {
             taskContainer.style.transform = `translateY(${subGroupHeight - 120}px)`
             let distance;
@@ -169,27 +175,24 @@ const checkTaskAnimation = (e,a) => {
             } else {
                 distance = startingPoint * 60 - 60;
             }
-            for (let i = startingPoint+1; i < subGroup.children.length; i++) {
+            for (let i = startingPoint; i < subGroup.children.length; i++) {
                 subGroup.children[i].style.transform = `translateY(${distance}px)`;
                 distance += 60;
             }    
-        },400)
-       setTimeout(()=> subGroup.appendChild(taskContainer),800)
+        },300)
     } 
 
-    else if (!checkedTask.className.includes('completed') && taskContainer != subGroup.children[1]) {
+    else if (checkedTask.className.includes('completed')) {
+        console.log('hi')
+        subGroup.insertBefore(taskContainer, subGroup.children[1]);
         setTimeout(()=> {
             taskContainer.style.transform = `translateY(0px)`
             let distance2 = 60;
-            for (let i = 1 ; i < startingPoint; i++) {
+            for (let i = 2 ; i < startingPoint+1; i++) {
                 subGroup.children[i].style.transform = `translateY(${distance2}px)`;
                 distance2 += 60;
             }    
-        },400)
-        setTimeout(()=> {
-            taskContainer.remove();
-            subGroup.insertBefore(taskContainer, subGroup.children[1]);
-        },800)
+        },300)
     }   
 }
 
